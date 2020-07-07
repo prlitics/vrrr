8c12cb19fcea20eba67e23f1cfa590ce5ca90e06
PRL <prlicari13@gmail.com>
2020-07-07 15:34:54 -0400

    Tidying and Making Helper Lists file

--- building_package/experimenting and fiddling.R
+++ building_package/experimenting and fiddling.R
@@ -5,7 +5,7 @@ library(yaml)
 
 
 vrrr_read <- function(path=getwd(), states, vtr_file=T, standard_columns = T, recode_vals = T) {
-  
+  source("C:/Users/prlic/Documents/GitHub/vrrr/building_package/helpful_lists.R")
   check_state(states = states)
   
   print(path)
@@ -25,7 +25,7 @@ states5<-c(1,"pie",3)
 
 
 
-vrrr_read(states = states4)
+vrrr_read(states = states1)
 
 
 
@@ -36,19 +36,7 @@ check_state<-function(states){
   if (typeof(states)=="character"){
     num_states<-length(states)
       if(num_states > 0){
-        state_list<-c("alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut",
-                      "delaware", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa",
-                      "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan",
-                      "minnesota", "mississippi","missouri","montana","nebraska","nevada","new hampshire",
-                      "newhampshire", "new jersey", "newjersey", "new mexico", "newmexico", "new york", 
-                      "newyork", "north carolina","northcarolina", "north dakota", "northdakota","ohio",
-                      "pennsylvania", "rhode island", "rhodeisland", "south carolina", "southcarolina",
-                      "south dakota", "southdakota", "tennessee", "texas", "utah", "vermont", "virginia",
-                      "washington", "west virginia", "westvirginia", "wisconsin", "wyoming", "al", "ak",
-                      "az", "ar", "ca", "co", "ct", "de", "fl", "ga", "hi", "id", "il", "in", "ia","ks",
-                      "ky","la","me","md","ma","mi","mn","ms","mo","mt","ne","nv","nh","nj","nm","ny",
-                      "nc","nd", "oh", "ok","or","pa","ri","sc","sd","tn","tx","ut","vt","va","wa","wv",
-                      "wi","wy")
+       
         for(i in states){
             if(i %in% state_list){
               print("Good test!")
