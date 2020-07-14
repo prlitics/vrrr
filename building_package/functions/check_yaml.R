a96538e43dd52d1a5b60428fb94c1c0a841a85d5
Peter Licari <prlicari13@gmail.com>
2020-07-14 14:13:36 -0400

    Rough Draft of load_state

--- building_package/functions/check_yaml.R
+++ building_package/functions/check_yaml.R
@@ -2,13 +2,13 @@ library(yaml)
 
 # function will take in a state 
 check_yaml <- function(state){
-  source("C:/Users/jenna/Documents/GitHub/vrrr/building_package/helpful_lists.R")
+  source("C:/Users/prlic/Documents/GitHub/vrrr/building_package/helpful_lists.R")
     # check if given a character vector
   if (typeof(state)=='character'){
     if (tolower(state) %in% names(state_key)){
       # continue 
       current_dir <- getwd()
-      setwd('C:/Users/jenna/Documents/GitHub/vrrr/yaml_files')
+      setwd('C:/Users/prlic/Dropbox/YAML/yaml standardization/yaml files/finished yaml')
       # paste .yaml on the backend of it 
       state_yaml <- paste0(state_key[tolower(state)],".yaml")
     
@@ -17,11 +17,10 @@ check_yaml <- function(state){
       # access a list of all YAMLs in our repo
       # check if this state is in it/exists
       if(file.exists(state_yaml)){
-        print("File exists, proceed")
         called_yaml<- yaml:: yaml.load_file(state_yaml)
       
         setwd(current_dir)
-        called_yaml
+        
       }
       
     }else{
@@ -31,7 +30,8 @@ check_yaml <- function(state){
   else{
     stop("Please insert a character string with your state's name")
   }
+  return(called_yaml)
 }
 
 
-check_yaml('florida')
+state_yaml<-check_yaml(7)
---
+++ building_package/functions/load_state(rough).R
@@ -0,0 +1,51 @@
+library(yaml)
+library(rlang)
+library(readr)
+
+
+vrrr_read <- function(path=getwd(), state, vtr_file=T, standard_columns = T, recode_vals = T)
+
+
+load_state<-function(state, vtr_file, state_yaml){
+
+
+yaml <- state_yaml
+
+# Gather Column Types
+
+cols<-yaml["column_classes"][[1]]
+col_names<-names(cols)
+
+x<-""
+z<-0
+for (i in cols) {
+  z<- z + 1
+  
+  if(z==1){
+    x<-(paste0('cols(',col_names[z]," = col_",i[[1]],'(), '))
+  }else if (z < length(cols)){
+    x<-paste0(x,col_names[z]," = col_",i[[1]],'(), ')}
+  else{
+    x <-paste0(x,col_names[z]," = col_",i[[1]],'() ',')')
+  }
+  
+
+  
+}
+
+
+# Gather Delimiter
+delim <- yaml[["delimiter"]]
+
+y<-parse_expr(x)
+
+
+#Load it in (need to complicate it from here, but it'll be fine for )
+readr::read_delim(file = vtr_file, delim = delim, col_names = T, col_types = eval(y))
+
+
+}
+
+h<-"hi"
+p<-parse_expr("print(h)")
+eval(p)
