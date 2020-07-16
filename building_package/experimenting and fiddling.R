5aaef916591a6472a212405b5a890430ad65681e
Peter Licari <prlicari13@gmail.com>
2020-07-16 15:32:49 -0400

    load_state() revisions; cleaning fiddling.R
    Adjusted load_state() to manipulate dates properly; repackaged most of the code doing the heavy lifting into a sub-function (simple_case()) that can be looped through when we deal with counties.

--- building_package/experimenting and fiddling.R
+++ building_package/experimenting and fiddling.R
@@ -9,7 +9,7 @@ vrrr_read <- function(path=getwd(), state, vtr_file, standard_columns = T, recod
   
   state_yaml<-check_yaml(state=state)
   
-  load_state(state, vtr_file = vtr_file, state_yaml = state_yaml)
+  load_state(vtr_file = vtr_file, state_yaml = state_yaml)
 
   
 }
@@ -24,42 +24,10 @@ states5<-c(1,"pie",3)
 states6 <-c("fLoRiDa")
 
 
-shQuote("test")
 
 
-ak<-vrrr_read(state = "ak", vtr_file = "C:/Users/prlic/Downloads/VOTERS List Report 9.25.19.csv")
+ak<-vrrr_read(state = , vtr_file = "C:/Users/prlic/Downloads/VOTERS List Report 9.25.19.csv")
 
-
-check_state<-function(states){
-  # Check if given a character vector
-  if (typeof(states)=="character"){
-    num_states<-length(states)
-    
-    if(num_states == 1){
-      #Check if given a state that is in the list of available state YAMLs
-      if(tolower(states) %in% state_list){
-        #If so, yay!
-        
-        
-      } else { 
-        #too many states
-        stop("Your state input is either not a state in our library or is not in a recognized format.")
-      }
-      
-    } else if(num_states > 1){
-      stop("vrr_read can only read-in one state at a time.")
-      
-    } else {
-      
-      stop("Please enter at least one state or state postal abbreviation.")
-    }
-    
-    
-  } else{
-    
-    stop("Please insert a character list of at least one state or state postal abbreviation.")
-  }
-}
 
 
 
@@ -67,7 +35,7 @@ check_state<-function(states){
 yaml<-yaml.load_file("C:/Users/prlic/Dropbox/YAML/yaml standardization/yaml files/finished yaml/alaska.yaml")
 
 
-
+yaml[["format"]][["separate_counties_voter_file"]]
 
 
 
--- building_package/functions/load_state(rough).R
+++ building_package/functions/load_state(rough).R
@@ -1,7 +1,7 @@
 library(yaml)
 library(rlang)
 library(readr)
-
+library(tibble)
 
 list.files(pattern="get the file ending from the yaml")
 
@@ -11,43 +11,72 @@ load_state<-function(vtr_file, state_yaml){
 
 yaml <- state_yaml
 
+# Gather Date Format
+date_format<-yaml[["date_format"]]
+
+#Gather County
+agg_county<-yaml[["format"]][["separate_counties_voter_file"]]
+
+
 # Gather Column Types
 
 cols<-yaml["column_classes"][[1]]
 col_names<-names(cols)
 
-x<-""
-z<-0
-for (i in cols) {
-  z<- z + 1
-  
-  if(z==1){
-    x<-(paste0('list(',shQuote(col_names[z])," = col_",i[[1]],'(), '))
-  }else if (z < length(cols)){
-    x<-paste0(x,shQuote(col_names[z])," = col_",i[[1]],'(), ')}
-  else{
-    x <-paste0(x,shQuote(col_names[z])," = col_",i[[1]],'() ',')')
-  }
-  
-
-  
-}
-
-
 # Gather Delimiter
 delim <- yaml[["delimiter"]]
 
-y<-parse_expr(x)
 
-print(names(y))
+simple_case<-function(){
+  x<-""
+  z<-0
+  for (i in cols) {
+    
+    z<- z + 1
+    
+    if(i == "date"){
+      
+      if(z==1){
+        x<-(paste0('list(',shQuote(col_names[z])," = col_date( format = ",shQuote(date_format),'),'))
+      }else if (z < length(cols)){
+        x<-paste0(x,shQuote(col_names[z])," = col_date( format = ",shQuote(date_format),'),')}
+      else{
+        x <-paste0(x,shQuote(col_names[z])," = col_date( format = ",shQuote(date_format),')',')')
+      }
+      
+    } else {
+      
+      if(z==1){
+        x<-(paste0('list(',shQuote(col_names[z])," = col_",i[[1]],'(), '))
+      }else if (z < length(cols)){
+        x<-paste0(x,shQuote(col_names[z])," = col_",i[[1]],'(), ')}
+      else{
+        x <-paste0(x,shQuote(col_names[z])," = col_",i[[1]],'() ',')')
+      }
+    }
+    
+    
+    
+  }
+  y<-parse_expr(x)
+  readr::read_delim(file = vtr_file, delim = delim, col_names = T, col_types = eval(y)) 
+  
+  
+}
 
 
-#Load it in (need to complicate it from here, but it'll be fine for )
-readr::read_delim(file = vtr_file, delim = delim, col_names = T, col_types = eval(y))
+if(isTRUE(agg_county)){
+  
+  print("Lol we'll deal with this later")
+
+  }else{
+
+  simple_case()
+
+  }
+
 
 
 }
 
-h<-"hi"
-p<-parse_expr("print(h)")
-eval(p)
+
