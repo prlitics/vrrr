0ca4646d6798b673c8af109d3a01a40b5c2fc876
tingum <68029412+tingum@users.noreply.github.com>
2020-07-20 14:58:09 -0400

    standardize_cols

---
+++ building_package/functions/standardize_cols.R
@@ -0,0 +1,31 @@
+library(yaml)
+library(rlang)
+library(readr)
+library(tibble)
+
+list.files(pattern="get the file ending from the yaml")
+
+
+standardize_cols<-function(vtr_file, state_yaml){
+  
+  yaml <- state_yaml
+  
+  # Gather Column Types
+  cols<-yaml["column_classes"][[1]]
+  col_names<-names(cols)
+  
+  # Gather Column Names
+  cols<-yaml["standard_colpairs"][[1]]
+  col_names <- names(cols)
+
+  # if column names gathered here, match the original column names 
+  if(col_names == colnames(vtr_file)){
+    # then gather standard colnames
+    standard_cols = c()
+    for(i in 1:length(cols)){
+      standard_cols = append(standard_cols, cols[[i]])
+    }
+    colnames(utah.file) = standard_cols
+  }
+  
+}
