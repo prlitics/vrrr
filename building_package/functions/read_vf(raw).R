20494feb23292a75540c76e7821b0dcd8b81757b
Peter Licari <prlicari13@gmail.com>
2020-08-15 09:32:00 -0400

    Adding yamls; function renames
    renaming functions and adding data

--- building_package/functions/read_vf(raw).R
+++ building_package/functions/read_vf(raw).R
@@ -1,12 +1,14 @@
 
 
-read_vf<- function(path=getwd(), state, vtr_file = NULL, standard_columns = TRUE, recode_vals = TRUE
+read_vf<- function(path=getwd(), state, vtr_file = NULL, standard_columns = TRUE, recode_vals = TRUE,
                    yaml = NULL) {
   check_state(states = state)
 
-  state_yaml<-check_yaml(state=state)
+  state_yaml<-retrieve_yaml(state=state)
 
-  load_state(path=path, vtr_file = vtr_file, state_yaml = state_yaml)
-
+  #load_state(path=path, vtr_file = vtr_file, state_yaml = state_yaml)
+  print(state_yaml)
 
 }
+
+read_vf(state = "fL")
